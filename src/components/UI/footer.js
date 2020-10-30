fc2a3210f4852ae3d693b0bfc4955f61d8668b79
Meynor R Lucas <andre.rlucas@outlook.com>
2020-10-29 18:42:02 -0600

    content: footer logo control desk/mob

--- src/components/UI/footer.js
+++ src/components/UI/footer.js
@@ -1,31 +1,39 @@
 import React from 'react'
 import { Text, Flex, Box, Image } from 'rebass'
+import { GlobalContainer } from 'store'
+import { SvgIcon } from './icon'
 
-const Footer = ({ userTheme }) => (
-  <Box as="footer" width={1} p={3}>
-    <Flex 
-      justifyContent="space-between"
-      flexDirection="column"
-      alignItems="center"
-      maxWidth="1600px"
-      mx="auto"
-    >
-      <Box as="figure" maxWidth="820px">
-        <Image 
-          src={`/images/chaingraph_logo_big-${userTheme === 'light' ? 'l' : 'd'}.png`}
-          width={1}
-        />
-      </Box>
-      <Text as="span"
-        fontWeight="300"
-        fontSize={1}
+const Footer = ({ userTheme }) => {
+  const { mobile } = GlobalContainer.useContainer()
+
+  return (
+    <Box as="footer" width={1} p={3}>
+      <Flex 
+        justifyContent="space-between"
+        flexDirection="column"
+        alignItems="center"
+        maxWidth="1600px"
         mx="auto"
-        opacity={0.75}
       >
-        © {new Date().getFullYear()}, all rights reserved.
-      </Text>
-    </Flex>
-  </Box>
-)
+        <Box as="figure" maxWidth="820px">
+        {
+          mobile ?
+            <SvgIcon icon={`chaingraph_logo-${userTheme === 'light' ? 'l' : 'd'}-footer-m`} />
+          :
+            <Image src={`/images/chaingraph_logo_big-${userTheme === 'light' ? 'l' : 'd'}.png`} width={1} alt="chainGraph_logo_footer" />
+        }
+        </Box>
+        <Text as="span"
+          fontWeight="300"
+          fontSize={1}
+          mx="auto"
+          opacity={0.75}
+        >
+          © {new Date().getFullYear()}, all rights reserved.
+        </Text>
+      </Flex>
+    </Box>
+  )
+}
 
 export default Footer
